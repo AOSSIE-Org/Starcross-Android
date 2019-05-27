a4922f7ec294236d156d1018a55aa9151a4d131f
makhanovable <makhanovable@gmail.com>
2019-05-27 11:54:23 +0600

    setup renderer, import data

---
+++ app/src/main/java/org/aossie/starcross/renderer/SkyRenderer.java
@@ -0,0 +1,30 @@
+package org.aossie.starcross.renderer;
+
+import android.opengl.GLSurfaceView;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+import static android.opengl.GLES20.glClearColor;
+import static android.opengl.GLES20.glClear;
+import static android.opengl.GLES20.glViewport;
+import static android.opengl.GLES20.GL_COLOR_BUFFER_BIT;
+
+public class SkyRenderer implements GLSurfaceView.Renderer {
+
+    @Override
+    public void onSurfaceCreated(GL10 glUnused, EGLConfig config) {
+        glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 glUnused, int width, int height) {
+        glViewport(0, 0, width, height);
+    }
+
+    @Override
+    public void onDrawFrame(GL10 glUnused) {
+        glClear(GL_COLOR_BUFFER_BIT);
+    }
+
+}
