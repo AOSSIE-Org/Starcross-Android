aa74f55f6ec1613d78cbcd65ea0994a828005432
makhanovable <makhanovable@gmail.com>
2019-06-10 08:57:37 +0600

    sensor model

---
+++ app/src/main/java/org/aossie/starcross/sensors/SensorModelAdaptor.java
@@ -0,0 +1,19 @@
+package org.aossie.starcross.sensors;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+
+public class SensorModelAdaptor implements SensorEventListener {
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        // TODO
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // TODO
+    }
+
+}
