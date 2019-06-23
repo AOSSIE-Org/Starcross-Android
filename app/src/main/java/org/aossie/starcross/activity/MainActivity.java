94c7ce9b146c19f07326055b9a5903ea155efdd3
makhanovable <makhanovable@gmail.com>
2019-06-23 12:57:27 +0600

    planets layer

--- app/src/main/java/org/aossie/starcross/activity/MainActivity.java
+++ app/src/main/java/org/aossie/starcross/activity/MainActivity.java
@@ -12,6 +12,7 @@ import org.aossie.starcross.control.AstronomerModelImpl;
 import org.aossie.starcross.control.ControllerGroup;
 import org.aossie.starcross.layer.ConstellationsLayer;
 import org.aossie.starcross.layer.LayerManager;
+import org.aossie.starcross.layer.PlanetsLayer;
 import org.aossie.starcross.layer.StarsLayer;
 import org.aossie.starcross.renderer.RendererController;
 import org.aossie.starcross.renderer.RendererModelUpdateClosure;
@@ -52,7 +53,9 @@ public class MainActivity extends AppCompatActivity {
 
         LayerManager layerManager = new LayerManager();
         layerManager.addLayer(new StarsLayer(getAssets(), getResources()));
-//        layerManager.addLayer(new ConstellationsLayer(getAssets(), getResources())); // TODO render constellations
+        // TODO filter works with adding or removing layer From layer manager
+//        layerManager.addLayer(new ConstellationsLayer(getAssets(), getResources())); // TODO add constellations to surfaceview
+//        layerManager.addLayer(new PlanetsLayer(getResources())); // TODO add planets to surfaceview
         layerManager.initialize();
         layerManager.registerWithRenderer(rendererController);
 
