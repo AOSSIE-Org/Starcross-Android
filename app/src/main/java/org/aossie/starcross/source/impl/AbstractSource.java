b4c7fc609db8612106d85745653495ff2bc71888
makhanovable <makhanovable@gmail.com>
2019-08-21 08:38:21 +0600

    sourse impl

--- app/src/main/java/org/aossie/starcross/source/impl/AbstractSource.java
+++ app/src/main/java/org/aossie/starcross/source/impl/AbstractSource.java
@@ -1,18 +1,43 @@
 package org.aossie.starcross.source.impl;
 
+import android.graphics.Color;
+
+import org.aossie.starcross.source.data.Colorable;
 import org.aossie.starcross.source.data.PositionSource;
 import org.aossie.starcross.util.GeocentricCoordinates;
 
-public abstract class AbstractSource implements PositionSource {
+import java.util.List;
+
+public abstract class AbstractSource implements PositionSource, Colorable {
+    private final int color;
     private final GeocentricCoordinates xyz;
+    private List<String> names;
 
     @Deprecated
     AbstractSource() {
-        this(GeocentricCoordinates.getInstance(0.0f, 0.0f));
+        this(GeocentricCoordinates.getInstance(0.0f, 0.0f), Color.BLACK);
     }
 
-    AbstractSource(GeocentricCoordinates coords) {
+    protected AbstractSource(int color) {
+        this(GeocentricCoordinates.getInstance(0.0f, 0.0f), color);
+    }
+
+    protected AbstractSource(GeocentricCoordinates coords, int color) {
         this.xyz = coords;
+        this.color = color;
+    }
+
+    public List<String> getNames() {
+        return names;
+    }
+
+    public void setNames(List<String> names) {
+        this.names = names;
+    }
+
+    @Override
+    public int getColor() {
+        return color;
     }
 
     @Override
--- app/src/main/java/org/aossie/starcross/source/impl/PointSourceImpl.java
+++ app/src/main/java/org/aossie/starcross/source/impl/PointSourceImpl.java
@@ -8,24 +8,23 @@ public class PointSourceImpl extends AbstractSource implements PointSource {
     private final int size;
     private Shape pointShape;
 
-    public PointSourceImpl(GeocentricCoordinates coords, int size) {
-        super(coords);
-        this.size = size;
-    }
-
     public PointSourceImpl(GeocentricCoordinates coords, int color, int size) {
         this(coords, color, size, Shape.CIRCLE);
     }
 
     public PointSourceImpl(GeocentricCoordinates coords, int color, int size, Shape pointShape) {
-        super(coords);
+        super(coords, color);
         this.size = size;
         this.pointShape = pointShape;
     }
 
-
     @Override
     public int getSize() {
         return size;
+    }
+
+    @Override
+    public Shape getPointShape() {
+        return pointShape;
     }
 }
