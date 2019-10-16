f0ce1a7646d2cc0fd7c90bcc6b72f9532fadd345
Makhanov Madiyar <makhanovable@gmail.com>
2019-10-16 14:48:16 +0600

    manually generated data added

---
+++ app/src/main/java/org/aossie/starcross/util/DATAGEN.java
@@ -0,0 +1,104 @@
+package org.aossie.starcross.util;
+
+import android.content.res.Resources;
+
+import org.aossie.starcross.R;
+
+public class DATAGEN {
+    public static String getLabel(Resources resource, int id) {
+        String label;
+        switch (id) {
+            case 2131558779: label = resource.getString(R.string.orion); break;
+            case 2131558563: label = resource.getString(R.string.gemini); break;
+            case 2131558446: label = resource.getString(R.string.cancer); break;
+            case 2131558449: label = resource.getString(R.string.canis_minor); break;
+            case 2131558448: label = resource.getString(R.string.canis_major); break;
+            case 2131558757: label = resource.getString(R.string.monoceros); break;
+            case 2131558597: label = resource.getString(R.string.lepus); break;
+            case 2131558857: label = resource.getString(R.string.sextans); break;
+            case 2131558810: label = resource.getString(R.string.pyxis); break;
+            case 2131558903: label = resource.getString(R.string.triangulum); break;
+            case 2131558435: label = resource.getString(R.string.aries); break;
+            case 2131558594: label = resource.getString(R.string.leo); break;
+            case 2131558595: label = resource.getString(R.string.leo_minor); break;
+            case 2131558620: label = resource.getString(R.string.lynx); break;
+            case 2131558921: label = resource.getString(R.string.virgo); break;
+            case 2131558918: label = resource.getString(R.string.vela); break;
+            case 2131558460: label = resource.getString(R.string.centaurus); break;
+            case 2131558496: label = resource.getString(R.string.crater); break;
+            case 2131558427: label = resource.getString(R.string.antlia); break;
+            case 2131558583: label = resource.getString(R.string.hydra); break;
+            case 2131558809: label = resource.getString(R.string.puppis); break;
+            case 2131558467: label = resource.getString(R.string.columba); break;
+            case 2131558455: label = resource.getString(R.string.carina); break;
+            case 2131558456: label = resource.getString(R.string.cassiopeia); break;
+            case 2131558790: label = resource.getString(R.string.pictor); break;
+            case 2131558536: label = resource.getString(R.string.dorado); break;
+            case 2131558423: label = resource.getString(R.string.andromeda); break;
+            case 2131558592: label = resource.getString(R.string.lacerta); break;
+            case 2131558882: label = resource.getString(R.string.taurus); break;
+            case 2131558437: label = resource.getString(R.string.auriga); break;
+            case 2131558578: label = resource.getString(R.string.horologium); break;
+            case 2131558443: label = resource.getString(R.string.caelum); break;
+            case 2131558793: label = resource.getString(R.string.piscis_austrinus); break;
+            case 2131558831: label = resource.getString(R.string.sculptor); break;
+            case 2131558462: label = resource.getString(R.string.cetus); break;
+            case 2131558557: label = resource.getString(R.string.fornax); break;
+            case 2131558789: label = resource.getString(R.string.phoenix); break;
+            case 2131558905: label = resource.getString(R.string.tucana); break;
+            case 2131558444: label = resource.getString(R.string.camelopardalis); break;
+            case 2131558550: label = resource.getString(R.string.eridanus); break;
+            case 2131558784: label = resource.getString(R.string.pegasus); break;
+            case 2131558787: label = resource.getString(R.string.perseus); break;
+            case 2131558792: label = resource.getString(R.string.pisces); break;
+            case 2131558431: label = resource.getString(R.string.aquarius); break;
+            case 2131558908: label = resource.getString(R.string.ursa_major); break;
+            case 2131558814: label = resource.getString(R.string.reticulum); break;
+            case 2131558537: label = resource.getString(R.string.draco); break;
+            case 2131558461: label = resource.getString(R.string.cepheus); break;
+            case 2131558463: label = resource.getString(R.string.chamaeleon); break;
+            case 2131558466: label = resource.getString(R.string.circinus); break;
+            case 2131558468: label = resource.getString(R.string.coma_berenices); break;
+            case 2131558492: label = resource.getString(R.string.corona_australis); break;
+            case 2131558493: label = resource.getString(R.string.corona_borealis); break;
+            case 2131558497: label = resource.getString(R.string.crux); break;
+            case 2131558759: label = resource.getString(R.string.musca); break;
+            case 2131558494: label = resource.getString(R.string.corvus); break;
+            case 2131558447: label = resource.getString(R.string.canes_venatici); break;
+            case 2131558499: label = resource.getString(R.string.cygnus); break;
+            case 2131558501: label = resource.getString(R.string.delphinus); break;
+            case 2131558909: label = resource.getString(R.string.ursa_minor); break;
+            case 2131558549: label = resource.getString(R.string.equuleus); break;
+            case 2131558570: label = resource.getString(R.string.grus); break;
+            case 2131558576: label = resource.getString(R.string.hercules); break;
+            case 2131558584: label = resource.getString(R.string.hydrus); break;
+            case 2131558585: label = resource.getString(R.string.indus); break;
+            case 2131558923: label = resource.getString(R.string.vulpecula); break;
+            case 2131558598: label = resource.getString(R.string.libra); break;
+            case 2131558619: label = resource.getString(R.string.lupus); break;
+            case 2131558621: label = resource.getString(R.string.lyra); break;
+            case 2131558739: label = resource.getString(R.string.mensa); break;
+            case 2131558753: label = resource.getString(R.string.microscopium); break;
+            case 2131558904: label = resource.getString(R.string.triangulum_australe); break;
+            case 2131558770: label = resource.getString(R.string.norma); break;
+            case 2131558783: label = resource.getString(R.string.pavo); break;
+            case 2131558778: label = resource.getString(R.string.ophiuchus); break;
+            case 2131558883: label = resource.getString(R.string.telescopium); break;
+            case 2131558454: label = resource.getString(R.string.capricornus); break;
+            case 2131558922: label = resource.getString(R.string.volans); break;
+            case 2131558830: label = resource.getString(R.string.scorpius); break;
+            case 2131558832: label = resource.getString(R.string.scutum); break;
+            case 2131558823: label = resource.getString(R.string.sagitta); break;
+            case 2131558824: label = resource.getString(R.string.sagittarius); break;
+            case 2131558429: label = resource.getString(R.string.apus); break;
+            case 2131558432: label = resource.getString(R.string.aquila); break;
+            case 2131558852: label = resource.getString(R.string.serpens_cauda); break;
+            case 2131558776: label = resource.getString(R.string.octans); break;
+            case 2131558433: label = resource.getString(R.string.ara); break;
+            case 2131558851: label = resource.getString(R.string.serpens_caput); break;
+            case 2131558441: label = resource.getString(R.string.bootes); break;
+            default: label = ""; break;
+        }
+        return label;
+    }
+}
